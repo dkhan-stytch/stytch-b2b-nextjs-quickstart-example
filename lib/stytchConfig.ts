3bf80568f057b72ebd93ad896e4b4395de2cb92e
dkhan-stytch <dkhan@stytch.com>
2025-05-28 13:18:24 -0700

    Email OTP as MFA

--- lib/stytchConfig.ts
+++ lib/stytchConfig.ts
@@ -41,7 +41,7 @@ export const adminPortalConfig = {
 
 export const discoveryConfig = {
   authFlowType: AuthFlowType.Discovery,
-  products: [B2BProducts.sso, B2BProducts.oauth, B2BProducts.emailMagicLinks],
+  products: [B2BProducts.sso, B2BProducts.oauth, B2BProducts.emailMagicLinks, B2BProducts.passwords],
   sessionOptions: {
     sessionDurationMinutes: 60,
   },
